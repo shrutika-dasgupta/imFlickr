b32758bf01a542eba5819c0a623a1acfa3bc3020
Shrutika Dasgupta <sd2841@columbia.edu>
2014-10-03 14:44:57 -0400

    Handled the edge Cases, Improved the UI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var Flickr = require('flickrapi');
 
 /*--------------- All Routes ------------------------*/
 app.get('/', function(req, res){
-	res.render('template3' );
+	res.render('home_page' );
 });
 
 function twoDigits(d) 
@@ -25,7 +25,7 @@ Date.prototype.toMysqlFormat = function()
     	+ twoDigits(this.getUTCSeconds());
 };
 
-app.post('/test', function(request,result){
+app.post('/photo_results', function(request,result){
 
 
 	//var query = request.body.search_query;
@@ -36,19 +36,28 @@ app.post('/test', function(request,result){
 	var min_upload_date = null;
 	var max_upload_date = null; 
 
+	
 	var search_query_init = request.body.search_query;
 	search_query = search_query_init.split(' ').join('+');
 	console.log(search_query);	
+	
+	
 	
 	if(request.body.user_name != "")
 	{
 		user_name = (request.body.user_name).split(' ').join('+');
 		console.log("user: "+user_name);
 	}
-	if(request.body.min_upload_date != null)
+	if(request.body.min_upload_date != "")
 		min_upload_date = new Date(request.body.min_upload_date).toMysqlFormat();
-	if(request.body.max_upload_date != null)
+	if(request.body.max_upload_date != "")
 		max_upload_date = new Date(request.body.max_upload_date).toMysqlFormat();
+
+	if(search_query == "" && user_name == null && min_upload_date == null && max_upload_date == null)
+	{
+		console.log("wrong_query");
+		 return result.redirect("/?invalid_inputs");
+	}
 
 
 	console.log("Inside the query");
@@ -74,81 +83,30 @@ app.post('/test', function(request,result){
 			},function(err, res) {
 				if(res)
 				{
-					console.log(res);
-					var photo_urls= new Array(res.photos.photo.length);
-					var users_urls= new Array(res.photos.photo.length);
-					for (var i = res.photos.photo.length - 1; i >= 0 && i < 500; i--) {
-						photo_urls[i] = "https://farm"+res.photos.photo[i].farm+".staticflickr.com/"+res.photos.photo[i].server+"/"+res.photos.photo[i].id+"_"+res.photos.photo[i].secret+"_";
-						console.log(photo_urls[i]);
-						users_urls[i] = "http://www.flickr.com/people/"+res.photos.photo[i].owner+"/";
-						console.log(users_urls[i]);
-					};
-					result.render('test', {urls: photo_urls, users: users_urls});
+					console.log("no user photo res: ", res);
+					if(res.photos.total == 0){
+						return result.redirect("/?no_results");
+					}
+					else
+					{
+						var photo_urls= new Array(res.photos.photo.length);
+						var users_urls= new Array(res.photos.photo.length);
+						for (var i = res.photos.photo.length - 1; i >= 0 && i < 500; i--) {
+							photo_urls[i] = "https://farm"+res.photos.photo[i].farm+".staticflickr.com/"+res.photos.photo[i].server+"/"+res.photos.photo[i].id+"_"+res.photos.photo[i].secret+"_";
+							console.log(photo_urls[i]);
+							users_urls[i] = "http://www.flickr.com/people/"+res.photos.photo[i].owner+"/";
+							console.log(users_urls[i]);
+						};
+						result.render('photo_results', {urls: photo_urls, users: users_urls});
+					}
 				}
 				else
 				{
+					return result.redirect("/?invalid_inputs");
 					console.log("Query is not found");
 				}
 			});
 		});
-
-		/*Flickr.tokenOnly(flickrOptions,function(error, flickr) {
-
-			flickr.photos.search({
-				min_upload_date: min_upload_date,
-				max_upload_date: max_upload_date,
-				text: search_query,
-				page: 1,
-				per_page: 500
-			},function(err_p, res_p) {
-				if(res_p)
-				{
-					var user_profile = new Array(res.photos.photo.length);
-					var photo_user_id = new Array(res.photos.photo.length);
-
-					var photo_urls= new Array(res.photos.photo);
-					for (var i = res_p.photos.photo.length - 1; i >= 0 && i < 500; i--) {
-
-						photo_user_id[i] = res_p.photos.photo[i].owner;
-					};
-
-					for (var i = photo_user_id.length - 1 && i < 500; i >= 0; i--) {
-
-						Flickr.tokenOnly(flickrOptions,function(error, flickr) {
-
-							flickr.urls.getUserProfile({
-								user_id: photo_user_id[i],
-								
-							},function(err_up, res_up) {
-								if(res_up)
-								{
-									user_profile[i] = res_up.user.url;
-									console.log(res_up.user.url);
-								}
-								else
-								{
-									console.log("user not found");
-								}
-								console.log(user_profile);
-							});
-						});
-					};
-
-					for (var i = res.photos.photo.length - 1; i >= 0 && i < 500; i--) {
-						photo_urls[i] = "https://farm"+res.photos.photo[i].farm
-						+".staticflickr.com/"+res.photos.photo[i].server
-						+"/"+res.photos.photo[i].id+"_"
-						+res.photos.photo[i].secret+"_m.jpg";
-					}
-					result.render('test', {urls: photo_urls});
-					
-				}
-				else
-				{
-					console.log("Query is not found");
-				}
-			});
-		});*/
 	}
 	else
 	{
@@ -160,6 +118,7 @@ app.post('/test', function(request,result){
 			},function(err, res) {
 				if(res)
 				{
+					console.log("no user res: ", res);
 					var user_id = res.user.id;
 
 					Flickr.tokenOnly(flickrOptions,function(error, flickr) {
@@ -174,18 +133,26 @@ app.post('/test', function(request,result){
 						},function(err, res) {
 							if(res)
 							{
-								var photo_urls= new Array(res.photos.photo.length);
-								var users_urls= new Array(res.photos.photo.length);
-								for (var i = res.photos.photo.length - 1; i >= 0 && i < 500; i--) {
-									photo_urls[i] = "https://farm"+res.photos.photo[i].farm+".staticflickr.com/"+res.photos.photo[i].server+"/"+res.photos.photo[i].id+"_"+res.photos.photo[i].secret+"_";
-									users_urls[i] = "http://www.flickr.com/people/"+res.photos.photo[i].owner+"/";
-									console.log(users_urls[i]);
-								};
-								result.render('test', {urls: photo_urls, users: users_urls});
+								console.log("user photo res: ", res);
+								if(res.photos.total == 0){
+									return result.redirect("/?no_results");
+								}
+								else
+								{
+									var photo_urls= new Array(res.photos.photo.length);
+									var users_urls= new Array(res.photos.photo.length);
+									for (var i = res.photos.photo.length - 1; i >= 0 && i < 500; i--) {
+										photo_urls[i] = "https://farm"+res.photos.photo[i].farm+".staticflickr.com/"+res.photos.photo[i].server+"/"+res.photos.photo[i].id+"_"+res.photos.photo[i].secret+"_";
+										users_urls[i] = "http://www.flickr.com/people/"+res.photos.photo[i].owner+"/";
+										console.log(users_urls[i]);
+									};
+									result.render('photo_results', {urls: photo_urls, users: users_urls});
+								}
 							}
 							else
 							{
 								console.log("Query is not found");
+								return result.redirect("/?invalid_inputs");
 							}
 						});
 					});
@@ -193,23 +160,10 @@ app.post('/test', function(request,result){
 				else
 				{
 					console.log("User name is invalid");
-					result.redirect('/?invalid_username');
+					return result.redirect('/?invalid_username');
 				}
 			});
 		});
 	}
 });
 
-app.post('/loadNext', function(request, result){
-
-});
-
-//console.log("window variable: ", window.photos);
-
-app.get('/temp3', function(req, res){
-	res.render('template3' );
-});
-
-app.get('/temp1', function(req, res){
-	res.render('template');
-});
